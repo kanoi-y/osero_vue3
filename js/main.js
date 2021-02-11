1bdd965bebd3864e07e2ca00dd4fd619e534c3bc
kanoi-y <furafuratonbo@gmail.com>
2021-02-11 22:05:06 +0900

    オセロのおく場所のチェックをする処理を追加中

--- js/main.js
+++ js/main.js
@@ -1,8 +1,62 @@
 const App = {
-    data() {
-      return {
-      };
+  data() {
+    return {
+      putCount: 0,
+      nowPlayer: 2,
+      fields: [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 2, 0, 0, 0],
+        [0, 0, 0, 2, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+      ],
+    };
+  },
+  methods: {
+    panelClass(cell) {
+      if (cell === 1) {
+        return "white-panel";
+      }
+      if (cell === 2) {
+        return "black-panel";
+      }
+      return "";
     },
-  };
+    putPanel(i, j) {
+      const thisPanel = this.display[i][j];
+      if (thisPanel === 1 || thisPanel === 2) {
+        alert("ここにはおけないよ！");
+        return;
+      }
 
-  Vue.createApp(App).mount("#app");
+      let checkAround = false;
+
+      for (let h = -1; h < 2; h++) {
+        for (let v = -1; v < 2; v++) {
+          if (
+            (0 !== this.display[i + h][j + v]) &
+            (this.nowPlayer !== this.display[i + h][j + v])
+          ) {
+            checkAround = true;
+          }
+        }
+      }
+      console.log(checkAround);
+
+      if (checkAround === false) {
+        alert("ここにはおけないよ！");
+        return;
+      }
+    },
+  },
+  computed: {
+    display() {
+      if (this.putCount === 0) return this.fields;
+    },
+  },
+};
+
+Vue.createApp(App).mount("#app");
