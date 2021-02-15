83259704486bb76f09687edaa720e865edfaf711
kanoi-y <furafuratonbo@gmail.com>
2021-02-15 22:36:15 +0900

    npm-scriptsでscssのコンパイルを設定した

--- js/main.js
+++ js/main.js
@@ -26,13 +26,15 @@ const App = {
       return "";
     },
     putPanel(i, j) {
+
+      // この場所に駒があるかどうか
       const thisPanel = this.display[i][j];
       if (thisPanel === 1 || thisPanel === 2) {
         alert("ここにはおけないよ！");
         return;
       }
 
-      let checkAround = false;
+      let antiPanels = [];
 
       for (let h = -1; h < 2; h++) {
         for (let v = -1; v < 2; v++) {
@@ -40,16 +42,33 @@ const App = {
             (0 !== this.display[i + h][j + v]) &
             (this.nowPlayer !== this.display[i + h][j + v])
           ) {
-            checkAround = true;
+            antiPanels.push([h, v]);
           }
         }
       }
-      console.log(checkAround);
+      console.log(antiPanels);
 
-      if (checkAround === false) {
+      // 周囲に相手の駒があるかどうか
+      if (antiPanels.length === 0) {
         alert("ここにはおけないよ！");
         return;
       }
+
+      const CheckStraight = (panel, h, v) => {
+          
+        if ( this.nowPlayer !== this.display[i + panel[0] + h][j + panel[1] + v]) {
+          CheckStraight(panel, panel[0] + h, panel[1] + v);
+        }
+
+      }
+
+      antiPanels.forEach(antiPanel => {
+       
+      });
+
+
+
+
     },
   },
   computed: {
